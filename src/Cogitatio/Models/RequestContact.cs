025f45c7da821437b5d262796ca9b341c01218ea
Matt Raffel <matt.raffel@gmail.com>
2024-12-06 07:58:44 -0700

    contact page work
    Signed-off-by: Matt Raffel <matt.raffel@gmail.com>

---
+++ src/Cogitatio/Models/RequestContact.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cogitatio.Models;
+
+public class RequestContact
+{
+    [StringLength(maximumLength:75)] public string Name { get; set; }
+    [EmailAddress] public string Email { get; set; }
+    [StringLength(maximumLength:250)] public string Message { get; set; }
+}
